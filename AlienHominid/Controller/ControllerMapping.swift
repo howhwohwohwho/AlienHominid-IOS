43db9fdefeaf650eace6c052a82e89a00c1c6156
howhwohwohwho <149392086+howhwohwohwho@users.noreply.github.com>
2026-09-16 17:40:28 +0400

    Update ControllerMapping.swift

--- AlienHominid/Controller/ControllerMapping.swift
+++ AlienHominid/Controller/ControllerMapping.swift
@@ -2,8 +2,12 @@ import GameController
 
 enum GameButton {
     case jump
-    case attack
-    case special
+    case shoot
+    case melee
+    case enterVehicle
+    case throwGrenade
+    case rollRight
+    case rollLeft
     case pause
 }
 
@@ -19,17 +23,44 @@ final class ControllerMapping {
         }
 
         switch button {
+
         case .jump:
             return gamepad.buttonA.isPressed
 
-        case .attack:
+        case .shoot:
             return gamepad.buttonX.isPressed
 
-        case .special:
+        case .melee:
+            return gamepad.buttonX.isPressed
+
+        case .enterVehicle:
             return gamepad.buttonY.isPressed
+
+        case .throwGrenade:
+            return gamepad.buttonB.isPressed
+
+        case .rollRight:
+            return gamepad.rightTrigger.isPressed
+
+        case .rollLeft:
+            return gamepad.leftTrigger.isPressed
 
         case .pause:
             return gamepad.buttonMenu.isPressed
         }
+    }
+
+    func movement(
+        controller: GCController
+    ) -> (x: Float, y: Float) {
+
+        guard let gamepad = controller.extendedGamepad else {
+            return (0, 0)
+        }
+
+        return (
+            x: gamepad.leftThumbstick.xAxis.value,
+            y: gamepad.leftThumbstick.yAxis.value
+        )
     }
 }
