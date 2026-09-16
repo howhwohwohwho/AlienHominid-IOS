0dceb81a2178a8fd12fb67854048cadcd9869e8a
howhwohwohwho <149392086+howhwohwohwho@users.noreply.github.com>
2026-09-16 17:55:38 +0400

    Update GameLoop.swift

--- AlienHominid/Engine/GameLoop.swift
+++ AlienHominid/Engine/GameLoop.swift
@@ -6,10 +6,22 @@ final class GameLoop {
     private var displayLink: CADisplayLink?
     private var lastTimestamp: CFTimeInterval = 0
 
-    var update: ((TimeInterval) -> Void)?
+    private let engine: GameEngine
+    private let controllerManager: ControllerManager
+
+    init(
+        engine: GameEngine,
+        controllerManager: ControllerManager
+    ) {
+        self.engine = engine
+        self.controllerManager = controllerManager
+    }
 
     func start() {
         stop()
+
+        controllerManager.start()
+        engine.start()
 
         lastTimestamp = 0
 
@@ -18,25 +30,43 @@ final class GameLoop {
             selector: #selector(tick)
         )
 
-        link.add(to: .main, forMode: .common)
+        link.add(
+            to: .main,
+            forMode: .common
+        )
+
         displayLink = link
     }
 
     func stop() {
         displayLink?.invalidate()
         displayLink = nil
+
+        controllerManager.stop()
     }
 
-    @objc private func tick(_ link: CADisplayLink) {
+    @objc private func tick(
+        _ link: CADisplayLink
+    ) {
         if lastTimestamp == 0 {
             lastTimestamp = link.timestamp
             return
         }
 
-        let deltaTime = link.timestamp - lastTimestamp
+        let deltaTime =
+            link.timestamp - lastTimestamp
+
         lastTimestamp = link.timestamp
 
-        update?(deltaTime)
+        controllerManager.update()
+
+        if controllerManager.input.pause {
+            engine.pause()
+        }
+
+        engine.update(
+            deltaTime: deltaTime
+        )
     }
 
     deinit {
