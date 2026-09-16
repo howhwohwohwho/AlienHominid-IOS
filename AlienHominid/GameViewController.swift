d6c6224ba5b51486300991ce20b0a5fb4a07c6a0
howhwohwohwho <149392086+howhwohwohwho@users.noreply.github.com>
2026-09-16 21:33:24 +0400

    Update GameViewController.swift

--- AlienHominid/GameViewController.swift
+++ AlienHominid/GameViewController.swift
@@ -2,9 +2,37 @@ import UIKit
 
 final class GameViewController: UIViewController {
 
+    private var gameView: GameView!
+    private let gameEngine = GameEngine()
+
+    override func loadView() {
+        gameView = GameView(frame: .zero)
+        view = gameView
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = .black
+
+        gameEngine.start()
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+
+        gameEngine.resume()
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        true
+    }
+
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        .landscape
+    }
+
+    override var prefersHomeIndicatorAutoHidden: Bool {
+        true
     }
 }
