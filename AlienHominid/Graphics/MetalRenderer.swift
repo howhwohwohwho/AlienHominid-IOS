11580ad07d5be18099b4f6f4132d631dbee7d875
howhwohwohwho <149392086+howhwohwohwho@users.noreply.github.com>
2026-09-16 18:09:58 +0400

    Update MetalRenderer.swift

--- AlienHominid/Graphics/MetalRenderer.swift
+++ AlienHominid/Graphics/MetalRenderer.swift
@@ -3,9 +3,6 @@ import MetalKit
 final class MetalRenderer: NSObject, MTKViewDelegate {
 
     private let device: MTLDevice
-    private let commandQueue: MTLCommandQueue
-
-    let textureManager: TextureManager
     private let spriteRenderer: SpriteRenderer
     private let testTextureGenerator: TestTextureGenerator
 
@@ -13,15 +10,12 @@ final class MetalRenderer: NSObject, MTKViewDelegate {
 
     init?(view: MTKView) {
         guard let device = view.device,
-              let commandQueue = device.makeCommandQueue(),
               let spriteRenderer = SpriteRenderer(device: device)
         else {
             return nil
         }
 
         self.device = device
-        self.commandQueue = commandQueue
-        self.textureManager = TextureManager(device: device)
         self.spriteRenderer = spriteRenderer
         self.testTextureGenerator = TestTextureGenerator(
             device: device
@@ -48,44 +42,14 @@ final class MetalRenderer: NSObject, MTKViewDelegate {
     }
 
     func draw(in view: MTKView) {
-        guard let drawable = view.currentDrawable,
-              let descriptor = view.currentRenderPassDescriptor,
-              let commandBuffer = commandQueue.makeCommandBuffer()
-        else {
+        guard let sprite = testSprite else {
             return
         }
 
-        if let attachment = descriptor.colorAttachments[0] {
-            attachment.loadAction = .clear
-            attachment.storeAction = .store
-
-            attachment.clearColor = MTLClearColor(
-                red: 0.05,
-                green: 0.05,
-                blue: 0.05,
-                alpha: 1.0
-            )
-        }
-
-        if let sprite = testSprite,
-           let texture = sprite.texture {
-
-            spriteRenderer.draw(
-                sprite: Sprite(
-                    texture: texture,
-                    position: sprite.position,
-                    size: sprite.size,
-                    rotation: sprite.rotation,
-                    visible: sprite.visible
-                ),
-                in: view
-            )
-
-            return
-        }
-
-        commandBuffer.present(drawable)
-        commandBuffer.commit()
+        spriteRenderer.draw(
+            sprite: sprite,
+            in: view
+        )
     }
 
     func mtkView(
